ccb08059a6e3299aea80989825deb9a3083a4684
drmcarvalho <furiano.k@gmail.com>
2024-06-14 22:20:28 -0300

    create database

--- src/FrmProjectOne.pas
+++ src/FrmProjectOne.pas
@@ -6,7 +6,12 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.Controls.Presentation, FMX.StdCtrls, UFrmTaskShow, UFrmProjShow,
-  UFrmReqShow, UFunctions, UTranslate;
+  UFrmReqShow, UFunctions, UTranslate, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.FMXUI.Wait,
+  FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteWrapper.Stat,
+  FireDAC.Phys.SQLiteDef, FireDAC.Phys.SQLite, Data.DB, FireDAC.Comp.Client,
+  FireDAC.DApt;
 
 type
   TFormProjectOne = class(TForm)
@@ -14,6 +19,8 @@ type
     btnTarefas: TButton;
     btnRequisitos: TButton;
     btnAttachments: TButton;
+    FDConnection: TFDConnection;
+    FDPhysSQLiteDriverLink1: TFDPhysSQLiteDriverLink;
     procedure btnTarefasClick(Sender: TObject);
     procedure btnProjetosClick(Sender: TObject);
     procedure btnRequisitosClick(Sender: TObject);
@@ -21,6 +28,7 @@ type
     procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
+    procedure CreateDatabase;
   public
     { Public declarations }
   end;
@@ -32,10 +40,66 @@ implementation
 
 {$R *.fmx}
 
+procedure TFormProjectOne.CreateDatabase;
+var query: TFDQuery;
+begin
+  /// CONNECTION DATABASE SQLITE
+
+  FDConnection.DriverName                := 'SQLITE';
+  FDConnection.Params.Values['Database'] := PathOfExecutable + 'projectone.db';
+  try
+    FDConnection.Open;
+  except on E: EDatabaseError do
+    ShowMessage('Error: ' + E.Message)
+  end;
+
+
+  query := TFDQuery.Create(nil);
+  try
+    with query do
+    begin
+      Connection := FDConnection;
+      SQL.Clear;
+      SQL.Add('CREATE TABLE IF NOT EXISTS "Projetos" (');
+      SQL.Add('	"Id" INTEGER NOT NULL,');
+      SQL.Add('	"Titulo" TEXT NOT NULL,');
+      SQL.Add('	"Corpo" TEXT NOT NULL,');
+      SQL.Add('	"Status" TEXT NOT NULL,');
+      SQL.Add('	PRIMARY KEY ("Id")');
+      SQL.Add(');');
+      SQL.Add('CREATE TABLE IF NOT EXISTS "Requisitos" (');
+      SQL.Add('	"Id" INTEGER NOT NULL,');
+      SQL.Add('	"ProjetoId" INTEGER NOT NULL,');
+      SQL.Add('	"Titulo" TEXT NOT NULL,');
+      SQL.Add('	"Descricao" TEXT NULL DEFAULT NULL,');
+      SQL.Add('	"Ativo" TINYINT NOT NULL,');
+      SQL.Add('	"Tipo" TEXT NOT NULL, "Status" TEXT NOT NULL DEFAULT "EA",');
+      SQL.Add('	PRIMARY KEY ("Id"),');
+      SQL.Add('	CONSTRAINT "fk_projeto_requisito" FOREIGN KEY ("ProjetoId") REFERENCES "Projetos" ("Id") ON UPDATE CASCADE ON DELETE CASCADE');
+      SQL.Add(');');
+      SQL.Add('CREATE TABLE IF NOT EXISTS "Tarefas" (');
+      SQL.Add('	"Id" INTEGER NOT NULL,');
+      SQL.Add('	"ProjetoId" INTEGER NOT NULL,');
+      SQL.Add('	"RequisitoId" INTEGER NOT NULL,');
+      SQL.Add('	"Titulo" TEXT NOT NULL,');
+      SQL.Add('	"Descricao" TEXT NULL,');
+      SQL.Add('	"Status" TEXT NOT NULL,');
+      SQL.Add('	PRIMARY KEY ("Id"),');
+      SQL.Add('	CONSTRAINT "fk_projeto_tarefa" FOREIGN KEY ("ProjetoId") REFERENCES "Projetos" ("Id") ON UPDATE CASCADE ON DELETE CASCADE,');
+      SQL.Add('	CONSTRAINT "fk_requisito_tarefa" FOREIGN KEY ("RequisitoId") REFERENCES "Requisitos" ("Id") ON UPDATE CASCADE ON DELETE CASCADE');
+      SQL.Add(');');
+      ExecSQL;
+    end;
+  finally
+    query.Close;
+    query.DisposeOf;
+  end;
+end;
+
 procedure TFormProjectOne.btnAttachmentsClick(Sender: TObject);
 begin
-  //ShowMessage(PathOfExecutable);
-  ShowMessage(_t('', PtSelectRecord));
+ShowMessage(PathOfExecutable);
+//  ShowMessage(_t('', PtSelectRecord));
 end;
 
 procedure TFormProjectOne.btnProjetosClick(Sender: TObject);
@@ -64,7 +128,14 @@ end;
 
 procedure TFormProjectOne.FormCreate(Sender: TObject);
 begin
+  /// Translate experimental
+
   InitTranslate;
+
+
+  /// CREATE FILE SQLITE AND TABLES
+
+  CreateDatabase;
 end;
 
 end.
